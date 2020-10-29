11f0e74558be7230f39f4b46eaf6dfe09d12c85f
rebirth111 <1066324750@qq.com>
2020-10-29 22:43:31 +0800

    react-router-dom路由基础

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-
+import {Route,Link,NavLink} from 'react-router-dom'
+import { Redirect, Switch } from 'react-router-dom/cjs/react-router-dom.min';
 import './App.css';
 import Home from './components/Home'
+import Phone from './components/phone'
+import User from './components/User'
 function App() {
   return (
     <div className="App">
-      你好
-      <Home />
+      <div>
+        <NavLink to="/home">点我去home</NavLink>
+        <NavLink to="/phone">点我去phone</NavLink>
+        <NavLink to="/user">点我去user</NavLink>
+      </div>
+      <Switch>
+      <Route path="/" exact component={Home}/>
+      <Route path="/home" component={Home}/>
+      <Route path="/phone" component={Phone}/>
+      <Route path="/user" component={User}/>
+      <Route path="/user" component={User}/>
+      <Redirect from="/" to="/home/homea" exact />
+      </Switch>
     </div>
   );
 }
