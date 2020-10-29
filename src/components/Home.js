f23c609e79b2533f69772d67eecaf18dec39a27f
rebirth111 <1066324750@qq.com>
2020-10-29 20:43:02 +0800

    模拟数据和数据请求

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,26 +1,31 @@
 import React, { Component } from 'react'
-import News from './News'
-import Phone from './phone'
+import axios from 'axios'
 export default class Home extends Component {
     constructor(props){
         super(props)
         this.state={
-            text:"我是默认值"
+            arr:[]
         }
     }
-    dataFun=(text)=>{
-        console.log(text)
-        this.setState({
-            text
+    componentDidMount(){
+        this.ajaxfun()
+    }
+    ajaxfun=()=>{
+        axios.get("http://localhost:4000/arr").then((ok)=>{
+            console.log(ok)
+            this.setState({
+                arr:ok.data
+            })
         })
     }
     render() {
         return (
             <div>
-                home----{this.state.text}
-                <News text="你好" fufun={this.dataFun}/>
-                <Phone />
+                {this.data.arr.map((v,i)=>{
+                    return <p key={i}>{v.name}</p>
+                })}
             </div>
         )
     }
 }
+
